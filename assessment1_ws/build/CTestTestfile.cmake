0b120cc0ed8b451b2840aeb36cbb7d488423e1fe
Sean Dougan <86353559+Txndra@users.noreply.github.com>
2026-03-03 15:23:20 +0000

    Committed all initial files. Map created and SLAM done

---
+++ assessment1_ws/build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/xat/csed2/assessment1_ws/src
+# Build directory: /home/xat/csed2/assessment1_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("path_planning")
+subdirs("pp_msgs")
+subdirs("custom_gazebo_world")
+subdirs("srv_client_plugin")
+subdirs("turtlebot3_model")
---
+++ assessment1_ws/build/srv_client_plugin/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/xat/csed2/assessment1_ws/src/srv_client_plugin
+# Build directory: /home/xat/csed2/assessment1_ws/build/srv_client_plugin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
